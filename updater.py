940b6b20b4af0b874d881fe747af0e8a01529026
TheInvisibleFoe <sabarnohere@gmail.com>
2023-12-12 00:52:39 +0530

    Tue Dec 12 00:52:39 2023

--- updater.py
+++ updater.py
@@ -2,4 +2,7 @@ import os
 import datetime
 loc_hypr = "~/.config/hypr"
 os.system("cp -r "+loc_hypr+" ../dotfiles")
-# os.system("git commit -m "+"")
+os.system("git add . ")
+x = datetime.datetime.now()
+os.system("git commit -m "+"\""+str(x.strftime("%c"))+"\"")
+os.system("git push origin main")
