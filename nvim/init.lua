3c6eabe0ebf2f16d6af75d915d8507e125bc4563
sab <sabarnohere@gmail.com>
2025-03-15 16:05:02 +0530

    Sat Mar 15 16:05:02 2025

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,2 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("vim-options")
-require("lazy").setup("plugins")
-require 'lspconfig'.typst_lsp.setup {
-    settings = {
-        exportPdf = "onType" -- Choose onType, onSave or never.
-        -- serverPath = "" -- Normally, there is no need to uncomment it.
-    }
-}
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
